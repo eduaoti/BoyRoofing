b70e23b48dd4b368a4a8120282415b3f2270e06e
boar61666 <gearss@gmail.com>
2025-11-27 10:47:00 -0600

    Solucion reseñas duplicadas

--- Frontend-BoysRoofing/src/app/(public)/en/page.tsx
+++ Frontend-BoysRoofing/src/app/(public)/en/page.tsx
@@ -1,12 +1,5 @@
 import HomePage from "@/components/HomePage";
-import ReviewsCarousel from "@/components/ReviewsCarousel";
 
 export default function PageEN() {
-  return (
-    <>
-      <HomePage />
-      <ReviewsCarousel lang="en" />
-    </>
-  );
+  return <HomePage />;
 }
-
